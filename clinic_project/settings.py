1d9ac9d25aece69305bd05b6408659bd746db651
Alauddin <a.faya@fintechsys.net>
2026-01-05 21:45:13 +0300

    feat: allow all hosts by setting ALLOWED_HOSTS to wildcard

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -119,6 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # CORS Settings
 CORS_ALLOW_ALL_ORIGINS = True
